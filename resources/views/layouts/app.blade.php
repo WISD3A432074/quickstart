55ea8686a916f73410a7a70da307bafc983c6b49
Your Name <you@example.com>
2017-10-11 00:32:19 +0800

    [3A432074]add bootstrap to views

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,15 +4,30 @@
     <title>Laravel 快速入門 - 基本</title>
 
     <!-- CSS 及 JavaScript -->
+    <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" rel="stylesheet">
 </head>
 
 <body>
-<div class="container">
-    <nav class="navbar navbar-default">
-        <!-- Navbar 內容 -->
-    </nav>
-</div>
+<body id="app-layout">
+<nav class="navbar navbar-default">
+    <div class="container">
+        <div class="navbar-header">
+
+            <!-- Branding Image -->
+            <a class="navbar-brand" href="{{ url('/') }}">
+                Task List
+            </a>
+        </div>
+
+    </div>
+</nav>
 
 @yield('content')
+
+<!-- JavaScripts -->
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
+{{-- <script src="{{ elixir('js/app.js') }}"></script> --}}
+</body>
 </body>
 </html>
