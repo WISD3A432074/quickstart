3bca336e51ff193319b131ccff0e21385a081fc4
Your Name <you@example.com>
2017-10-11 00:21:11 +0800

    [3A432074]add show tasks function to home route

--- routes/web.php
+++ routes/web.php
@@ -16,7 +16,11 @@ use Illuminate\Http\Request;
 use Illuminate\Validation\Validator;
 
 Route::get('/', function () {
-    return view('tasks');
+    $tasks = Task::orderBy('created_at', 'asc')->get();
+
+    return view('tasks', [
+        'tasks' => $tasks
+    ]);
 });
 
 /**
